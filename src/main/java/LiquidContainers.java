ea6c515c7126caa44d77523fa9d887cd41302849
CarlosAndresQuel00 <66259796+CarlosAndresQuel00@users.noreply.github.com>
2020-08-05 21:38:08 -0500

    Add files via upload

--- src/main/java/LiquidContainers.java
+++ src/main/java/LiquidContainers.java
@@ -5,7 +5,10 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int liquid = 0;
+        int liq = 0;
+        System.out.println("First: " + liquid + "/100");
+        System.out.println("Second: " + liq + "/100");
 
         while (true) {
             System.out.print("> ");
@@ -14,8 +17,62 @@ public class LiquidContainers {
             if (input.equals("quit")) {
                 break;
             }
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);
+            if (command.equals("add")) {
+                if (amount > 0) {
+                    liquid += amount;
+                    if (liquid <= 100) {
+                        System.out.println("First: " + liquid + "/100");
+                    } else {
+                        liquid = 100;
+                        System.out.println("First: " + liquid + "/100");
+                    }
+                    System.out.println("Second: " + liq + "/100");
+                } else {
+                    System.out.println("First: " + liquid + "/100");
+                    System.out.println("Second: " + liq + "/100");
+                }
+            } else if (command.equals("move")) {
+                if (amount > 0 && liquid != 0) {
+                    liquid -= amount;
+                    if (liquid < 0) {
+                        liq = amount + liquid;
+                        liquid = 0;
+                        System.out.println("First: " + liquid + "/100");
+                        System.out.println("Second: " + liq + "/100");
+                    } else {
+                        System.out.println("First: " + liquid + "/100");
+                        liq += amount;
+                        if (liq <= 100) {
+                            System.out.println("Second: " + liq + "/100");
+                        } else {
+                            liq = 100;
+                            System.out.println("Second: " + liq + "/100");
+                        }
+                    }
+                } else {
+                    System.out.println("First: " + liquid + "/100");
+                    System.out.println("Second: " + liq + "/100");
+                }
+            } else if (command.equals("remove")) {
+                if (amount > 0) {
+                    liq -= amount;
+                    if (liq < 0) {
+                        liq = 0;
+                        System.out.println("First: " + liquid + "/100");
+                        System.out.println("Second: " + liq + "/100");
+                    } else {
+                        System.out.println("First: " + liquid + "/100");
+                        System.out.println("Second: " + liq + "/100");
+                    }
+                } else {
+                    System.out.println("First: " + liquid + "/100");
+                    System.out.println("Second: " + liq + "/100");
+                }
+            }
 
         }
     }
-
 }
